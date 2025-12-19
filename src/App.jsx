b362cb45492886aa56a12d11991bcca073b65e2e
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-12-19 16:49:50 -0300

    feat: integrate Hero, About, and Services section into main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-// src/App.jsx
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './sections/Hero/Hero';
+import { Services } from './sections/Services/Services';
+import { About } from './sections/About/About';
+
 import './styles/global.css';
 
 function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <Navbar />
+      
       <main>
-        <Hero />
         {}
+        <Hero />
+        <About />
+        <Services />
+        
+        {/* As próximas seções entrarão aqui */}
       </main>
-    </>
+
+      {}
+      <footer style={{ padding: '40px', textAlign: 'center', opacity: 0.5 }}>
+        © 2025 N2 TECH - Sua Visão, Nossa Tecnologia.
+      </footer>
+    </div>
   );
 }
 
